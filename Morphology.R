9ef0108029374e40b976e755a0363e71d5d629cc
Jan Grzybowski <JanGrzybowski@outlook.com>
2016-12-01 01:51:50 +0100

    Optymalizacja dylatacji i erozji

--- Morphology.R
+++ Morphology.R
@@ -1,61 +1,52 @@
 source("Filters.R")
 ## ---- Dilation ----
 
-dilation <- function(layer, filtr = matrix(c(1,1,1,1,NA,1,1,1,1), nrow =3, ncol=3), repetitions  = 1){
-  filter2H = floor(getHeight(filtr)/2)
-  filter2W = floor(getWidth(filtr)/2)
-  
+dilation <- function(layer, filtr = matrix(c(1,1,1,1,0,1,1,1,1), nrow =3, ncol=3), repetitions  = 1){
   maxH = getHeight(layer)
   maxW = getWidth(layer)
   
-  args = expand.grid(1:maxH, 1:maxW)
-  midResult = layer
+  indexes = as.vector(matrix(1:(maxH*maxW),ncol = maxW, nrow = maxH)[1:(maxH), 1:(maxW)])
   
-  for(i in 1:repetitions)
-  {
-    resultLayer <- matrix(
-      apply(args, 1, 
-        function(hw){
-          hrange = ((hw[1]-filter2H) : (hw[1]+filter2H)) %>% clip(1, maxH)
-          wrange = ((hw[2]-filter2W) : (hw[2]+filter2W)) %>% clip(1, maxW)
-          dilate(midResult[hrange,wrange], filtr, midResult[hw[1],hw[2]])
-      }), nrow = maxH)
-    midResult = resultLayer
-  }
-  resultLayer
-}  
-
-dilate <- function(patch, pattern, v){
-  if((patch - pattern)[pattern == 1] %>% "=="(0) %>% na.omit %>% sum > 0)
-    1
-  else
-    v
+  dataIndexes = cbind(indexes-maxH-1, indexes-maxH, indexes-maxH+1,
+                      indexes-1, indexes+0, indexes+1, 
+                      indexes+maxH-1, indexes+maxH, indexes+maxH+1)
+  
+  dataIndexes[which(dataIndexes[,5] %% 5 == 1),c(1,4,7)] = NA
+  dataIndexes[which(dataIndexes[,5] %% 5 == 0),c(3,6,9)] = NA
+  
+  dataIndexes[dataIndexes<1] = NA
+  dataIndexes[dataIndexes>maxH*maxW] = NA
+  
+  windows = matrix(layer[dataIndexes], nrow=length(indexes))
+  windows[is.na(windows)] = FALSE
+  (windows[,which(filtr==1)] == 1) %>% rowSums() %>% matrix(nrow = maxH, ncol = maxW) %>% "+"(layer) %>% sign()
 }
 
 ## ---- Erosion ----
 
-erosion <-function(layer, filtr = matrix(c(1,1,1,1,NA,1,1,1,1), nrow =3, ncol=3), repetitions = 1){
-  filter2H = floor(getHeight(filtr)/2)
-  filter2W = floor(getWidth(filtr)/2)
-  
+erosion <-function(layer, filtr = matrix(c(1,1,1,1,0,1,1,1,1), nrow =3, ncol=3), repetitions = 1){
   maxH = getHeight(layer)
   maxW = getWidth(layer)
   
-  args = expand.grid(1:maxH, 1:maxW)
-  midResult = layer
+  indexes = as.vector(matrix(1:(maxH*maxW),ncol = maxW, nrow = maxH)[1:(maxH), 1:(maxW)])
   
-  for(i in 1:repetitions)
-  {
-    resultLayer <- matrix(
-      apply(args, 1, 
-            function(hw){
-              hrange = ((hw[1]-filter2H) : (hw[1]+filter2H)) %>% clip(1, maxH)
-              wrange = ((hw[2]-filter2W) : (hw[2]+filter2W)) %>% clip(1, maxW)
-              erode(midResult[hrange,wrange], filtr, midResult[hw[1],hw[2]])
-            }), nrow = maxH)
-    midResult = resultLayer
-  }
-  resultLayer
+  dataIndexes = cbind(indexes-maxH-1, indexes-maxH, indexes-maxH+1,
+                      indexes-1, indexes+0, indexes+1, 
+                      indexes+maxH-1, indexes+maxH, indexes+maxH+1)
+  
+  dataIndexes[which(dataIndexes[,5] %% 5 == 1),c(1,4,7)] = NA
+  dataIndexes[which(dataIndexes[,5] %% 5 == 0),c(3,6,9)] = NA
+  
+  dataIndexes[dataIndexes<1] = NA
+  dataIndexes[dataIndexes>maxH*maxW] = NA
+  
+  windows = matrix(layer[dataIndexes], nrow=length(indexes))
+  windows[is.na(windows)] = FALSE
+  leaveOriginaValuesIndexes = (windows[,which(filtr==1)] == 1) %>% rowSums() %>% "=="(sum(filtr)) %>% ">"(0)
+  
+  result = matrix(0,ncol = maxW, nrow = maxH)
+  result[leaveOriginaValuesIndexes] = layer[leaveOriginaValuesIndexes]
+  result
 }  
 
 erode <- function(patch, pattern, v){
@@ -82,6 +73,36 @@ opening <- function(image, repetitions = 1){
 }
 
 ## ---- Time ----
+test.dilation <- function(){
+  i = matrix(c(0,0,0,0,0, 0,1,0,0,0, 0,0,1,1,0, 1,0,0,1,1, 0,1,1,0,0), ncol=5)
+  fil = matrix(c(0,0,0,1,0,0,0,1,1), ncol=3)
+  expected <- matrix(c(1,1,0,0,0, 0,1,1,1,0, 1,0,1,1,1, 1,1,1,1,1, 0,1,1,1,0), ncol=5)
+  result = dilation(i,fil)
+  if(T == all.equal(result, expected))
+  {  }
+  else {
+    warning("Dilation TEST FAILED")
+    print(result)
+    print(expected)
+    print(expected-result)
+  }
+}
+
+test.erosion <- function(){
+  i = matrix(c(0,0,0,0,0, 0,1,0,0,0, 0,0,1,1,0, 1,0,0,1,1, 0,1,1,0,0), ncol=5)
+  fil = matrix(c(0,0,0,1,0,0,0,1,1), ncol=3)
+  expected <- matrix(c(0,0,0,0,0, 0,0,0,0,0, 0,0,0,1,0, 0,0,0,0,0, 0,0,0,0,0), ncol=5)
+  result = erosion(i,fil)
+  if(T == all.equal(result, expected))
+  {  }
+  else {
+    warning("Erosion TEST FAILED")
+    print(result)
+    print(expected)
+    print(expected-result)
+  }
+  
+}
 
 TimeTest <- function(fn){
   ptm = proc.time()
@@ -89,3 +110,6 @@ TimeTest <- function(fn){
   time =  proc.time() - ptm
   list("time" = time, "fnResults" = v)
 }
+
+test.dilation()
+test.erosion()
